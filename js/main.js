686ba85aa0ec64171225b636a60da77b17f0b2fd
Ocram7 <marco.ty.chen@gmail.com>
2021-12-27 22:35:32 +0800

    Enter

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+
+/* for when the user hovers over the user icon */
+function profile(){
+    document.getElementById("settings").style.display="block";
+    document.getElementById("myBooks").style.display="block";
+}
+
+function endProfile(){
+    document.getElementById("settings").style.display = "none";
+    document.getElementById("myBooks").style.display = "none"; 
+}
+
+
+/* Changing the search bar from text field to genre dropdown */
 function formChange(ele){
     console.log(ele.value);
     if (ele.value == "genreName"){
@@ -9,7 +23,4 @@ function formChange(ele){
     }
 }
 
-function genreSelect(){
-    alert("in function");
-    alert(children[1]);
-}
+
